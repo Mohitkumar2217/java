e3aef7edd4219203d75328aebc3a5f116f512f45
Mohit kumawat <mohitkumawat221004@gmail.com>
2025-05-27 16:00:25 +0530

    conditional statement

---
+++ ConditionalStatement/Escapes.java
@@ -0,0 +1,10 @@
+public class Escapes {
+    public static void main(String[] args) {
+        System.out.println("\"Hello Mohit\"");
+        System.out.println("\'Hello Mohit\'");
+        System.out.println("\\Hello Mohit\\");
+        System.out.println("Hello\tMohit");
+        System.out.println("Hello \bMohit\n");
+        System.out.println("\bHello\tMohit\n");
+    }
+}
---
+++ ConditionalStatement/ifElse.java
@@ -0,0 +1,12 @@
+public class ifElse {
+    public static void main(String[] args) {
+        boolean isMale = true;
+        String name = "Kiran";
+        if(isMale) {
+            System.out.println("Mr." + name);
+        }
+        else {
+            System.out.println("Ms." + name);
+        }
+    }
+}
