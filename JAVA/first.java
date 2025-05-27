e5d95f9cc2fce45973456c557b7a2d2bc71c0ac7
Mohitkumar2217 <mohitkumawat221004@gmail.com>
2025-05-27 10:32:34 +0530

    new update

--- JAVA/first.java
+++ JAVA/first.java
@@ -3,6 +3,7 @@ public class first {
     public static void main(String[] args) {
         System.out.print("Hello World");
         System.out.println(" Hello World");
-        System.out.print("Hello World");
-    }
-}
+        System.out.print("Hello World ");
+        System.out.println("Hello World!!");
+    };
+};
