f46aa18fec0142dbd097cc768767120a516a14d7
Mohit kumawat <mohitkumawat221004@gmail.com>
2025-05-27 15:57:39 +0530

    type casting

---
+++ TypeCast/explicit.java
@@ -0,0 +1,8 @@
+public class explicit {
+    public static void main(String[] args) {
+        float myFloat1 = 5;
+        System.out.println(myFloat1);//implicit type casting
+        int myFloat2 = (int)45.50f;
+        System.out.println(myFloat2);
+    }
+}
