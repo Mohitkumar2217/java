9e76480bdd56a738ee7693a053b21e7f0c96626e
Mohit kumawat <mohitkumawat221004@gmail.com>
2025-05-27 15:30:28 +0530

    Delete JAVA directory

--- JAVA/Array2D.java
+++
@@ -1,9 +0,0 @@
-public class Array2D {
-    public static void main(String[] args) {
-        int[][] arr = new int[2][4];
-        arr[0][0] = 4;
-        int[][] array = {{23},{23,34}};
-        System.out.println(arr[0][0]);
-        System.out.println(array[1][1]);
-    }
-}
--- JAVA/first.java
+++
@@ -1,9 +0,0 @@
-//  code for print "Hello World"
-public class first {
-    public static void main(String[] args) {
-        System.out.print("Hello World");
-        System.out.println(" Hello World");
-        System.out.print("Hello World ");
-        System.out.println("Hello World!!");
-    };
-};
