9beab46512d43a18c6d8ad8fa635e99de7a570e8
Mohitkumar2217 <mohitkumawat221004@gmail.com>
2025-05-27 09:46:52 +0530

    started studying java

---
+++ JAVA/first.java
@@ -0,0 +1,8 @@
+//  code for print "Hello World"
+public class first {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+        System.out.println(" Hello World");
+        System.out.print("Hello World");
+    }
+}
